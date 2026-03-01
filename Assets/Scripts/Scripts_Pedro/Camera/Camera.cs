3255d3698967fa560710a8a5a6ae151b3df33222
Haru <pedroterezzino19@gmail.com>
2026-03-01 17:19:20 -0300

    missão de npc, câmera implementada, redirecionamento pra tela de game over.

---
+++ Assets/Scripts/Scripts_Pedro/Camera/Camera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+    public float velocidade = 0.1f;
+    public float cameraZ = -10f;
+    public Vector2 offset;
+
+    private void LateUpdate()
+    {
+        if (player == null) return;
+
+        Vector3 targetPos = new Vector3(
+            player.position.x + offset.x,
+            player.position.y + offset.y,
+            cameraZ
+        );
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, velocidade);
+    }
+}
