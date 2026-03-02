8c1837ae83a7b16dbf915f9ce1fa66656ef893cd
Haru <pedroterezzino19@gmail.com>
2026-03-02 16:00:45 -0300

    camera bounds implementado

--- Assets/Scripts/Scripts_Pedro/Camera/Camera.cs
+++ Assets/Scripts/Scripts_Pedro/Camera/Camera.cs
@@ -7,6 +7,22 @@ public class CameraGulosa : MonoBehaviour
     public float cameraZ = -10f;
     public Vector2 offset;
 
+    [Header("Camera Bounds")]
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    private Camera cam;
+    private float halfHeight;
+    private float halfWidth;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        halfHeight = cam.orthographicSize;
+        halfWidth = halfHeight * cam.aspect;
+    }
+
     private void LateUpdate()
     {
         if (player == null) return;
@@ -17,6 +33,20 @@ public class CameraGulosa : MonoBehaviour
             cameraZ
         );
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, velocidade);
+        float clampedX = Mathf.Clamp(
+            targetPos.x,
+            minBounds.x + halfWidth,
+            maxBounds.x - halfWidth
+        );
+
+        float clampedY = Mathf.Clamp(
+            targetPos.y,
+            minBounds.y + halfHeight,
+            maxBounds.y - halfHeight
+        );
+
+        Vector3 clampedPosition = new Vector3(clampedX, clampedY, cameraZ);
+
+        transform.position = Vector3.Lerp(transform.position, clampedPosition, velocidade);
     }
 }
